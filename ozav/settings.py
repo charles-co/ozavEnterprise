a818b2a3dd15146f6fb530dfb63944fec00c44e0
charles-co <charlesboy49@gmail.com>
2020-09-18 13:16:16 +0100

    updated files

--- ozav/settings.py
+++ ozav/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(__file__, os.pardir))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 
 
